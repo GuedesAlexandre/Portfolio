8f4db00e52cfbdc22f2b6bb237ed5aed0485d873
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-05-23 19:42:12 +0200

    cc

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+console.log(jQuery(window).scrollTop());
+
+$( document ).ready(function() {
+    jQuery(window).scroll(function () {
+
+	    if (jQuery(window).scrollTop() == 0)
+	        console.log('Scroll en haut de page');
+
+	    else if (jQuery(window).height() + jQuery(window).scrollTop() == jQuery(document).height())
+	        console.log('Scroll en bas de page');
+
+});
+jQuery(window).scroll(function () {
+    if ((jQuery("window").scrollTop() + jQuery(window).height() - $("footer").height()) > $(".arrow").height()){
+        $(".arrow").toggleClass("arrow-bottom");
+    }else{
+        $(".arrow").toggleClass(".arrow");
+    }
+})
+let X =  window.innerHeight;
+let Y = window.innerWidth;
+console.log(X);
+console.log(Y);
+
+$("footer");
+
+
+
+console.log($("footer").height());
+$(".arrow");
+console.log($(".arrow").height());
+let heightofarrow = $(".arrow").height();
+let heightoffooter = $("footer").height();
+console.log($("footer").position());
+
+})
