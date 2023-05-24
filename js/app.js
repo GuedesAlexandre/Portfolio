2e06244895dccf8fe93bb56eecff6e1b587cfadf
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2023-05-24 11:16:20 +0200

    tab

--- js/app.js
+++ js/app.js
@@ -1,38 +1,36 @@
-
 console.log(jQuery(window).scrollTop());
 
-$( document ).ready(function() {
+$(document).ready(function () {
     jQuery(window).scroll(function () {
 
-	    if (jQuery(window).scrollTop() + $("header").height() == 0)
-	        $(".arrow").toggleClass("arrow-bottom");
+        if (jQuery(window).scrollTop() + $("header").height() == 0)
+            $(".arrow").toggleClass("arrow-bottom");
 
-	    else if (jQuery(window).height() + jQuery(window).scrollTop() == jQuery(document).height())
-        $(".arrow").toggleClass("arrow-bottom");
-    
-
-});
-jQuery(window).scroll(function () {
-    if (((jQuery("window").scrollTop() + jQuery(window).height()) - $("footer").height()) > $(".arrow").height()){
-        $(".arrow").toggleClass("arrow-bottom");
-    }else{
-        $(".arrow").toggleClass(".arrow");
-    }
-})
-let X =  window.innerHeight;
-let Y = window.innerWidth;
-console.log(X);
-console.log(Y);
-
-$("footer");
+        else if (jQuery(window).height() + jQuery(window).scrollTop() == jQuery(document).height())
+            $(".arrow").toggleClass("arrow-bottom");
 
 
+    });
+    jQuery(window).scroll(function () {
+        if (((jQuery("window").scrollTop() + jQuery(window).height()) - $("footer").height()) > $(".arrow").height()) {
+            $(".arrow").toggleClass("arrow-bottom");
+        } else {
+            $(".arrow").toggleClass(".arrow");
+        }
+    })
+    let X = window.innerHeight;
+    let Y = window.innerWidth;
+    console.log(X);
+    console.log(Y);
 
-console.log($("footer").height());
-$(".arrow");
-console.log($(".arrow").height());
-let heightofarrow = $(".arrow").height();
-let heightoffooter = $("footer").height();
-console.log($("footer").position());
+    $("footer");
+
+
+    console.log($("footer").height());
+    $(".arrow");
+    console.log($(".arrow").height());
+    let heightofarrow = $(".arrow").height();
+    let heightoffooter = $("footer").height();
+    console.log($("footer").position());
 
 })
